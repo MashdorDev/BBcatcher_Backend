c7b0bf1d8cb5c6bc29e7b06baa5fc5a4747128fe
Dor <dorzairidev@gmail.com>
2023-10-12 13:43:41 -0400

    changed the stat-auth

--- api/start-auth.js
+++ api/start-auth.js
@@ -1,9 +1,12 @@
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-module.exports = async (req, res) => {
-  passport.authenticate('google', {
-    scope: ['openid', 'email', 'profile', 'https://www.googleapis.com/auth/calendar',
-      'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/tasks']
-  })(req, res);
-};
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.REDIRECT_URI,
+}, (token, tokenSecret, profile, done) => {
+  return done(null, profile);
+}));
+
+module.exports = passport.authenticate('google', { failureRedirect: '/' });
